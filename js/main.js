63748851a1daa4b460f07a49f98361b6257c387d
synthetictriggers <38343120+synthetictriggers@users.noreply.github.com>
2018-04-17 02:05:48 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@ jQuery(document).ready(function($){
 		setTimeout(function(){
 			var url = $('body').find('.redirectAnchor').attr('redirectUrl')
 			window.location.href = (url);
-		}, 500);
+		}, 2000);
 		var scriptURL = $('body').find('.redirectAnchor').attr('spreadsheetURL')
 		var payload = document.forms['survey'];
 		if (true) {
